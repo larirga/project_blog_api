65e6c798fe739fd8852a537371cb624fc64e4630
Larissa <larisssa_rga@hotmail.com>
2023-04-11 10:24:15 -0300

    [add] - endpoint GET /categories

--- src/routers/categoryRouter.js
+++ src/routers/categoryRouter.js
@@ -4,5 +4,6 @@ const categoryController = require('../controllers/category.controller');
 const categoryValidator = require('../middlewares/categoryValidator');
 
 router.post('/', tokenValidator, categoryValidator, categoryController.createCategory);
+router.get('/', tokenValidator, categoryController.getAllCategories);
 
 module.exports = router;
--- src/services/category.service.js
+++ src/services/category.service.js
@@ -2,6 +2,9 @@ const { Category } = require('../models');
 
 const createCategory = async (name) => Category.create({ name });
 
+const getAllCategories = async () => Category.findAll();
+
 module.exports = {
     createCategory,
+    getAllCategories,
 };
